ad951a39094f3feef971475e9421d23ff8602e25
BakkiavathyEzhumalai <bakkiavathy.e@gmail.com>
2017-01-07 00:49:24 -0500

    Initial HelloWorld application

---
+++ app.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class App extends React.Component {
+   render() {
+      return (
+         <div>
+			<h1>React Experimental Project</h1>
+			<h2>Hello World!!</h2>
+         </div>
+      );
+   }
+}
+
+export default App;
