c6a449cb735cebb0cee58e96e60d74d874fd8295
Valliappan, Gandhi <GANDHI_VALLIAPPAN@homedepot.com>
2017-01-10 23:54:05 -0500

    Changes to push to prod

--- webpack.config.js
+++ webpack.config.js
@@ -25,17 +25,7 @@ var config = {
             }
          }
       ]
-   },
-
-   devtool:"source-map",
-
-   plugins: [
-        new webpack.DefinePlugin({
-            'process.env': {
-                'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
-            }
-        }),
-    ]
+   }
 }
 
 module.exports = config;
