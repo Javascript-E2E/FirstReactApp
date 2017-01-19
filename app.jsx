7131ac735c1cdbd80d7f0d3ff6e5b0379979d1a2
Bakkiavathy Ezhumalai <bakkiavathy.e@gmail.com>
2017-01-19 09:15:38 -0500

    Implementing some more changes

--- app.jsx
+++ app.jsx
@@ -13,23 +13,36 @@ class App extends React.Component {
       }
 
       this.updateState = this.updateState.bind(this);
+	  this.changeFirstName = this.changeFirstName.bind(this);
+	  this.changeLastName = this.changeLastName.bind(this);
 
    };
 
    updateState(e) {
-	   var testFirstName = 'Bakkiavathy';
-	   var testLastName = 'Ezhumalai';
-	   var testData = this.state.data;
-	   testData.firstName = testFirstName;
-	   testData.lastName = testLastName;
-       this.setState({data: testData});
+	   var dataToSave = this.state.data;
+       this.setState({data: dataToSave});
+   }
+   
+   changeFirstName(e) {
+	   var firstName = e.target.value;
+	   var firstNameData = this.state.data;
+	   firstNameData.firstName = firstName;
+       this.setState({data: firstNameData});
+   }
+   
+   changeLastName(e) {
+	   var lastName = e.target.value;
+	   var lastNameData = this.state.data;
+	   lastNameData.lastName = lastName;
+       this.setState({data: lastNameData});
    }
 	
    render() {
       return (
          <div>
 			<h1>User Details</h1>
-			<Content contentProp = {this.state.data} updateStateProp = {this.updateState}/>
+			<Content contentProp = {this.state.data} updateStateProp = {this.updateState} 
+			changeFirstNameProp = {this.changeFirstName} changeLastNameProp = {this.changeLastName} />
          </div>
       );
    }
@@ -40,12 +53,12 @@ class Content extends React.Component {
        return (
           <div>
 			First Name:
-			<input type = "text" value = {this.props.contentProp.firstName} />
+			<input type = "text" onChange = {this.props.changeFirstNameProp}/>
 			<br/><br/>
 			Last Name: 
-			<input type = "text" value = {this.props.contentProp.lastName} />
+			<input type = "text" onChange = {this.props.changeLastNameProp} />
 			<br/><br/>
-			<button onClick = {this.props.updateStateProp}>GetNames</button>
+			<button onClick = {this.props.updateStateProp}>SAVE</button>
           </div>
        );
     }
