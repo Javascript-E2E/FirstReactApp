53849725c7a537858e0f5e2eceb830fc9fb52f10
Bakkiavathy Ezhumalai <bakkiavathy.e@gmail.com>
2017-01-19 21:33:22 -0500

    Some form changes+CSS

--- app.jsx
+++ app.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+var ReactCSSTransitionGroup = require('react-addons-css-transition-group');
+
 class App extends React.Component {
 	
 	constructor(props) {
@@ -12,13 +14,13 @@ class App extends React.Component {
 		 }
       }
 
-      this.updateState = this.updateState.bind(this);
+      this.saveDetails = this.saveDetails.bind(this);
 	  this.changeFirstName = this.changeFirstName.bind(this);
 	  this.changeLastName = this.changeLastName.bind(this);
 
    };
 
-   updateState(e) {
+   saveDetails(e) {
 	   var dataToSave = this.state.data;
        this.setState({data: dataToSave});
    }
@@ -40,8 +42,12 @@ class App extends React.Component {
    render() {
       return (
          <div>
-			<h1>User Details</h1>
-			<Content contentProp = {this.state.data} updateStateProp = {this.updateState} 
+			<ReactCSSTransitionGroup transitionName = "testtransition"
+			transitionAppear = {true} transitionAppearTimeout = {500}
+			transitionEnter = {false} transitionLeave = {false}>
+				<h1>User Details</h1>
+			</ReactCSSTransitionGroup>
+			<Content contentProp = {this.state.data} saveDetailsProp = {this.saveDetails} 
 			changeFirstNameProp = {this.changeFirstName} changeLastNameProp = {this.changeLastName} />
          </div>
       );
@@ -52,13 +58,19 @@ class Content extends React.Component {
     render() {
        return (
           <div>
-			First Name:
-			<input type = "text" onChange = {this.props.changeFirstNameProp}/>
-			<br/><br/>
-			Last Name: 
-			<input type = "text" onChange = {this.props.changeLastNameProp} />
-			<br/><br/>
-			<button onClick = {this.props.updateStateProp}>SAVE</button>
+			<form>
+				<label>
+					First Name:
+					<input type = "text" onChange = {this.props.changeFirstNameProp}/>
+				</label>
+				<br/><br/>
+				<label>
+					Last Name: 
+					<input type = "text" onChange = {this.props.changeLastNameProp} />
+				</label>
+				<br/><br/>
+				<button onClick = {this.props.saveDetailsProp}>SAVE</button>
+			</form>
           </div>
        );
     }
