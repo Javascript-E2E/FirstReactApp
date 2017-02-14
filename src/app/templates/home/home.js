9e1fdf92143ddccacc2c1643530282b18fe1934b
Azad Ramasamy <Azad Ramasamy>
2017-02-13 23:11:13 -0500

    Modify src code with Flux ------ INPROGRESS --------

--- src/app/templates/home/home.js
+++ src/app/templates/home/home.js
@@ -2,54 +2,78 @@ import React from 'react';
  import Content from './components/content';
  
  //var ReactCSSTransitionGroup = require('reactaddonscsstransitiongroup');
+
+function getHomeState(){
+    return {
+        data : {
+            firstName:'',
+            lastName:''
+        }
+    }
+}
  
- class Home extends React.Component {
+ var Home = React.createClass({
  	
- 	constructor(props) {
-       super(props);
+ 	// constructor(props) {
+    //    super(props);
  		
-       this.state = {
-          data: {
- 			 firstName:'',
- 			 lastName:''
- 		 }
-       }
+    //    this.state = {
+    //       data: {
+ 	// 		 firstName:'',
+ 	// 		 lastName:''
+ 	// 	 }
+    //    }
  
-       this.saveDetails = this.saveDetails.bind(this);
- 	  this.changeFirstName = this.changeFirstName.bind(this);
- 	  this.changeLastName = this.changeLastName.bind(this);
+    //    this.saveDetails = this.saveDetails.bind(this);
+ 	//   this.changeFirstName = this.changeFirstName.bind(this);
+ 	//   this.changeLastName = this.changeLastName.bind(this);
  
-    };
+    // },
+
+    getInitialState : function(){
+        return getHomeState();
+    },
  
-    saveDetails(e) {
- 	   var dataToSave = this.state.data;
-        this.setState({data: dataToSave});
-    }
+    // saveDetails(e) {
+ 	//    var dataToSave = this.state.data;
+    //     this.setState({data: dataToSave});
+    // },
     
-    changeFirstName(e) {
- 	   var firstName = e.target.value;
- 	   var firstNameData = this.state.data;
- 	   firstNameData.firstName = firstName;
-        this.setState({data: firstNameData});
- 	   console.log(firstNameData.firstName)
-    }
+    // changeFirstName(e) {
+ 	//    var firstName = e.target.value;
+ 	//    var firstNameData = this.state.data;
+ 	//    firstNameData.firstName = firstName;
+    //     this.setState({data: firstNameData});
+ 	//    console.log(firstNameData.firstName)
+    // },
     
-    changeLastName(e) {
- 	   var lastName = e.target.value;
- 	   var lastNameData = this.state.data;
- 	   lastNameData.lastName = lastName;
-        this.setState({data: lastNameData});
-    }
+    // changeLastName(e) {
+ 	//    var lastName = e.target.value;
+ 	//    var lastNameData = this.state.data;
+ 	//    lastNameData.lastName = lastName;
+    //     this.setState({data: lastNameData});
+    // },
  	
+    componentDidMount : function () {
+        HomeStore.addChangeListener(this._onChange);
+    },
+
+    componentWillUnMount : function() {
+        HomeStore.removeChangeListener(this._onChange);
+    },
+
+    _onChange: function() {
+        this.setState(getHomeState());
+    },
+
     render() {
        return (
           <div>
  			<h1>User Details</h1>
- 			<Content saveDetailsProp = {this.saveDetails} 
- 			changeFirstNameProp = {this.changeFirstName} changeLastNameProp = {this.changeLastName} />
+ 			 <Content data = {this.state.data} />
           </div>
        );
     }
- }
+ });
  
  export default Home;
